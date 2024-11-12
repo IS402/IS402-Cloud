17d265a993170a176b9ac7b7986673eee81253f0
chauthanhbinh19 <21521871@gm.uit.edu.vn>
2024-11-12 19:52:32 +0700

    Admin Product UI

--- frontend/src/pages/AdminProductPage/AdminProductPage.jsx
+++ frontend/src/pages/AdminProductPage/AdminProductPage.jsx
@@ -8,6 +8,9 @@ import {
   Modal,
   Form,
   Input,
+  message,
+  Upload,
+  Spin
 } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -88,6 +91,8 @@ const data = [
 const AdminProductPage = () => {
   const [isHovered, setIsHovered] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const [fileList, setFileList] = useState([]); 
 
   const showModal = () => {
     setIsModalVisible(true);
@@ -95,6 +100,25 @@ const AdminProductPage = () => {
 
   const handleCancel = () => {
     setIsModalVisible(false);
+  };
+  const beforeUpload = (file) => {
+    const isImage = file.type.startsWith("image/");
+    if (!isImage) {
+      message.error("Bạn chỉ có thể tải lên hình ảnh!");
+    }
+    return isImage;
+  };
+  const handleImageChange = (info) => {
+    if (info.fileList && info.fileList.length > 0) {
+      const newFileList = info.fileList.slice(-1);
+      setFileList(newFileList);
+      const file = newFileList[0];
+      const imageUrl = file.thumbUrl || URL.createObjectURL(file.originFileObj);
+      setImageUrl(imageUrl);
+    } else {
+      setFileList([]);
+      setImageUrl("");
+    }
   };
   return (
     <div style={{ display: "flex", rowGap: 20, flexDirection: "column" }}>
@@ -149,6 +173,87 @@ const AdminProductPage = () => {
             ]}
           >
             <Input.TextArea placeholder="Nhập mô tả sản phẩm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Giá"
+            name="productPrice"
+            rules={[{ required: true, message: "Vui lòng nhập giá tiền sản phẩm!" }]}
+          >
+            <Input placeholder="Nhập giá tiền sản phẩm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Giá đã được giảm"
+            name="productDiscountPrice"
+            // rules={[{ required: true, message: "Vui lòng nhập giá tiền sản phẩm!" }]}
+          >
+            <Input placeholder="Nhập giá tiền sản phẩm đã được giảm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Danh mục"
+            name="productCategory"
+            rules={[{ required: true, message: "Vui lòng nhập danh mục sản phẩm!" }]}
+          >
+            <Input placeholder="Nhập danh mục sản phẩm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Hãng sản xuất"
+            name="productBrand"
+            rules={[{ required: true, message: "Vui lòng nhập hãng sản xuất sản phẩm!" }]}
+          >
+            <Input placeholder="Nhập hãng sản xuất sản phẩm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Tồn kho"
+            name="productBrand"
+            rules={[{ required: true, message: "Vui lòng nhập hãng sản xuất sản phẩm!" }]}
+          >
+            <Input placeholder="Nhập hãng sản xuất sản phẩm" />
+          </Form.Item>
+
+          <Form.Item
+            label="Ảnh thương hiệu"
+            name="brandImage"
+            valuePropName="fileList"
+            getValueFromEvent={(e) => e && e.fileList}
+            // rules={[{ required: true, message: "Vui lòng chọn ảnh thương hiệu!" }]}
+          >
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+              <Upload
+                name="brandImage"
+                listType="picture-card"
+                beforeUpload={beforeUpload}
+                showUploadList={false}
+                onChange={handleImageChange}
+                style={{ width: '200px' }}
+              >
+                <Button 
+                  icon={<PlusOutlined />} 
+                  style={{
+                    margin: '0', 
+                    padding: '0 10px', 
+                    height: '32px', 
+                    fontSize: '14px',
+                    border:'none',
+                    boxShadow:'none',
+                    backgroundColor:'#FAFAFA'
+                  }}
+                >
+                  Chọn ảnh
+                </Button>
+              </Upload>
+            </div>
+
+            {/* Display Image Preview */}
+            {imageUrl && (
+              <div style={{ textAlign: "center", marginTop: "10px" }}>
+                <img src={imageUrl} alt="brand" style={{ width: '100%', maxWidth: '200px' }} />
+              </div>
+            )}
           </Form.Item>
 
           <Form.Item style={{ display: "flex", justifyContent: "center" }}>
